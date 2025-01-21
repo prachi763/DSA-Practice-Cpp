72d29e4bb8a1598ae35d890e118f64725d93f6ed
prachi763 <prachi7630@gmail.com>
2025-01-21 09:32:07 +0530

    new

---
+++ leetcode/duplicate.cpp
@@ -0,0 +1,14 @@
+// approach 1 
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int s = nums.size();
+        for(int i=0;i<s-1;i++){
+            for (int j=i+1;j<s;j++){
+                if(nums[i]==nums[j])
+                return true;
+            }
+        }
+        return false;
+    }
+};
--- maps.cpp
+++ maps.cpp
@@ -21,25 +21,35 @@ int main(){
     //     cout<<it.first<<" - "<<it.second<<endl;
     // }
 
-    int key1 = 4;
-    int cnt1 = sq1.count(key1);
-    cout << "Count of " << key1 << ": " <<
-    cnt1 << endl;
+    // int key1 = 4;
+    // int cnt1 = sq1.count(key1);
+    // cout << "Count of " << key1 << ": " <<
+    // cnt1 << endl;
 
     // map<int, int> sq2 = {{4, 16}, {5, 25}, {6,36}};
     //     for(int i=4;i<=sq2.size();i++){
     //     cout<<sq2[i]<<" ";
     // }
     
-    for(auto it : sq1){
-        cout<<it.first<<" - "<<sq1.count(it.first)<<endl;
-    }
-
-    if(sq1.find(4) != sq1.end()){
-        cout<<"True"<<endl;
+    // for(auto it : sq1){
+    //     cout<<it.first<<" - "<<sq1.count(it.first)<<endl;
+    // }
+    int num = 6;
+    if(sq1.find(num) == sq1.end()){
+        cout<<"Element doesn't exist"<<endl;
+        sq1.insert({num , num*num});
     }
     else{
-        cout<<"False";
+        cout<<"Element exists";
+    }
+
+    num = 6;
+    if(sq1.find(num) == sq1.end()){
+        cout<<"Element doesn't exist"<<endl;
+        sq1.insert({num , num*num});
+    }
+    else{
+        cout<<"Element exists";
     }
 
     return 0;
