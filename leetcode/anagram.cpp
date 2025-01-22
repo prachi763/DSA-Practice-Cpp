6d299c1e7be953d315ddac2abe3b4f4266b09119
prachi763 <prachi7630@gmail.com>
2025-01-22 12:45:17 +0530

    ana

---
+++ leetcode/anagram.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<map>
+#include<string>
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    map<char, int> m;
+    for(auto element : s){
+        if(m.find(element) == m.end()){
+            m.insert({element,1});
+        }
+        else {
+            m[element]++;
+        }
+    }
+    
+    for(auto element : s){
+        if(m.find(element) != m.end()){
+            m[element]--;
+        }
+    }
+
+    for(auto key : m){
+        if(key.second != 0 ){
+            return false;
+        }
+    }
+    return true;
+}
+
+
+
+int main(){
+    string s = "anagram", t = "nagaram";
+    cout<<"Anagram yes or no? (0/1): "<<(isAnagram(s,t)?"Yes":"No");
+    
+    return 0;
+}
