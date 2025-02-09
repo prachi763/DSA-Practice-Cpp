fd622120569ccc972ca004d2be132a28c353ef2d
prachi763 <prachi7630@gmail.com>
2025-02-09 22:56:31 +0530

    f

---
+++ leetcode/169_majElement.cpp
@@ -0,0 +1,51 @@
+#include<iostream> 
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+// BRUTE FORCE
+// int majorityElement(vector<int>& nums) {
+//     int n = nums.size();
+//     for(int val : nums){
+//         int freq = 0;
+//         for(int el : nums){
+//             if(el == val){
+//                 freq++;
+//             }
+//         }
+//         if(freq > n/2){
+//             return val;
+//         }
+//     }
+
+// }
+
+//OPTIMISED
+int majorityElement(vector<int>& nums){
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    int freq = 0, ans = nums[0];
+    for(int i=1; i<n;i++){
+        if(nums[i] == nums[i-1]){
+            freq++;
+        }
+        else{
+            freq = 1;
+            ans = nums[i];
+        }
+        if(freq > n/2){
+            return ans;
+        }
+    }
+    return ans;
+}
+
+
+
+
+int main(){
+    vector<int> v = {1,2,2,1,1};
+    cout<<majorityElement(v);
+
+    return 0;
+}
