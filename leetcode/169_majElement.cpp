643fc7a7eb34fb55752cb09f668ab45bd8cf97c2
prachi763 <prachi7630@gmail.com>
2025-02-14 23:20:37 +0530

    p

--- leetcode/169_majElement.cpp
+++ leetcode/169_majElement.cpp
@@ -21,20 +21,37 @@ using namespace std;
 // }
 
 //OPTIMISED
-int majorityElement(vector<int>& nums){
+// int majorityElement(vector<int>& nums){
+//     int n = nums.size();
+//     sort(nums.begin(), nums.end());
+//     int freq = 0, ans = nums[0];
+//     for(int i=1; i<n;i++){
+//         if(nums[i] == nums[i-1]){
+//             freq++;
+//         }
+//         else{
+//             freq = 1;
+//             ans = nums[i];
+//         }
+//         if(freq > n/2){
+//             return ans;
+//         }
+//     }
+//     return ans;
+// }
+
+// MOORE'S VOTING ALGO
+int majorityElement(vector<int>& nums) {
+    int freq = 0, ans = 0;
     int n = nums.size();
-    sort(nums.begin(), nums.end());
-    int freq = 0, ans = nums[0];
-    for(int i=1; i<n;i++){
-        if(nums[i] == nums[i-1]){
+    for(int i=0;i<n;i++){
+        if(freq == 0)
+            ans = nums[i];
+        if(ans == nums[i]){
             freq++;
         }
         else{
-            freq = 1;
-            ans = nums[i];
-        }
-        if(freq > n/2){
-            return ans;
+            freq--;
         }
     }
     return ans;
